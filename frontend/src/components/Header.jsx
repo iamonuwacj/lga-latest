13ba3baf9a7e577035c1dadf69fedeb4e933747d
iamonuwacj <onuwachinedu66@gmail.com>
2025-01-30 11:23:34 +0100

    fixed the navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 
 const Header = () => {
 	const [toggle, setToggle] = useState(false)
+	const [dropdown, setDropdown] = useState(false)
+	const [dropdown1, setDropdown1] = useState(false)
 
   return (
 	<div className="px-[5%] py-6 bg-[#003758]  sm:w-full text-white flex  z-50
@@ -14,7 +16,7 @@ const Header = () => {
 			<span>logo</span>
 		</header>
 
-		<div className="flex w-[80%] justify-end">
+		<div className="flex w-[80%] justify-end ">
 			<ul className="hidden gap-14 sm:hidden md:hidden lg:flex">
 				<li><NavLink to={"/"}>Home</NavLink></li>
 
@@ -57,44 +59,45 @@ const Header = () => {
 			</ul>
 			{
 				toggle && (
-					<ul className="absolute  top-[4.5rem] w-full bg-green-900
-						left-0 space-y-10 py-10 pb-16
-						px-5 text-center lg:hidden">
-						<li><NavLink to={"/"}>Home</NavLink></li>
-						<li><NavLink>About</NavLink></li>
-						<li className="group relative ">
-							<p className='flex items-center gap-2 justify-center'>Departments <ChevronDown
-								className='group-hover:rotate-180 transition-all duration-500 ease-in-out'/>
+					<ul className="absolute top-[3.4rem] sm:top-[4rem] w-2/3 sm:w-1/2 bg-[#003758]
+						left-0  py-10
+						text-left space-y-5 lg:hidden">
+						<li className='bg-green-700 p-3'><NavLink to={"/"}>Home</NavLink></li>
+						<li className='hover:bg-green-700 p-3'><NavLink>About</NavLink></li>
+						<li className="group relative hover:bg-green-700 p-3" onClick={() => setDropdown(!dropdown)}
+							 onBlur={() =>setDropdown("")}>
+							<p className='flex items-center gap-2 justify-between'>Departments <ChevronDown
+								className={`transition-all duration-500 ease-in-out ${dropdown && "rotate-180"}`}/>
 							</p>
-
-							<div className="group-hover:block right-0 sm:right-[10%] md:right-[20%] absolute top-0 hidden h-auto opacity-1 
-								py-6 z-50">
-
-								<ul className="w-52 p-3 shadow bg-yellow-700 text-white rounded">
-									<li className="py-1 hover:ml-3 hover:text-gray-500">
-										Economic Development
-									</li>
-								</ul>
-							</div>
 						</li>
-						<li><NavLink>Carrers</NavLink></li>
+
+						{ dropdown && (
+							<li className='relative px-3'>
+								<ul>
+									<li className='hover:ml-16'>Economic Development</li>
+								</ul>
+						</li>
+						)}
+
+						<li className='hover:bg-green-700 p-3'><NavLink>Carrers</NavLink></li>
 						
-						<li className="group relative ">
-							<p className='flex items-center justify-center gap-2'>Pages <ChevronDown
+						<li className="group relative hover:bg-green-700 p-3" onClick={() => setDropdown1(!dropdown1)} onBlur={() =>setDropdown1("")}>
+							<p className='flex items-center justify-between gap-2'>Pages <ChevronDown
 								className='group-hover:rotate-180 transition-all duration-500 ease-in-out'/>
 							</p>
-
-							<div className="group-hover:block top-0 sm:right-[10%] md:right-[20%] right-0 absolute hidden h-auto opacity-1 py-6 z-50">
-
-								<ul className="w-52 p-3 shadow bg-yellow-700 text-white rounded">
+						</li>
+						{ dropdown1 && (
+							<li className='relative'>
+								<ul>
 									<li className="py-1 hover:ml-10 hover:text-gray-500"><NavLink>Single job</NavLink></li>
 									<li className="py-1 hover:ml-10 hover:text-gray-500"><NavLink>Team job</NavLink></li>
 								</ul>
-							</div>
-						</li>
-						<li><NavLink to={"/news"} reloadDocument>News</NavLink></li>
+							</li>
+						)}
+					
+						<li className='hover:bg-green-700 p-3'><NavLink to={"/news"} reloadDocument>News</NavLink></li>
 
-						<li className="relative block"><NavLink to={"/contact"}>Contacts</NavLink></li>
+						<li className="relative block hover:bg-green-700 p-3"><NavLink to={"/contact"}>Contacts</NavLink></li>
 					</ul>
 				)
 			}
